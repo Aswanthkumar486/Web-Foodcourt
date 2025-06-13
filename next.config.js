21c1ccefdad57e7eac49db03ca71c5dbf24bdee9
Your Name <you@example.com>
2025-06-13 12:58:19 +0530

    Deploy Next.js static build

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
     domains: ['images.unsplash.com'], // ✅ Allows loading from Unsplash
     unoptimized: true,                // ✅ Required for static export to skip Next Image optimization
   },
-  trailingSlash: true,        // ✅ Adds trailing slash to every route (e.g., /about/ instead of /about)
+  trailingSlash: true,  
+   output: "export",
+  basePath: "/Web-Foodcourt",
+  assetPrefix: "/Web-Foodcourt/"      // ✅ Adds trailing slash to every route (e.g., /about/ instead of /about)
 };
 
 module.exports = nextConfig;
